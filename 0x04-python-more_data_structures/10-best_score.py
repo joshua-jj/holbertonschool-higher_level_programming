861c39f6d45d738031cc2cf519b97bfb0ec3729f
alexaorrico <alexaorrico@gmail.com>
2017-08-10 20:23:58 +0000

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(my_dict):
+    if my_dict:
+        max = list(my_dict.values())[0]
+        for key in my_dict:
+            if my_dict[key] > max:
+                max = my_dict[key]
+        return max
+    return None
