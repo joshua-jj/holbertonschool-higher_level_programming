00d21dc6277b2f513e6927f5ba4fa10c18388d34
alexaorrico <alexaorrico@gmail.com>
2017-08-10 20:29:43 +0000

    check for empty dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict:
+    if my_dict and len(my_dict):
         max = list(my_dict.values())[0]
         for key in my_dict:
             if my_dict[key] > max:
