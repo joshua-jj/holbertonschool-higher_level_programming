11384dbcb9e008dca6e203561151282b9e4d8e03
alexaorrico <alexaorrico@gmail.com>
2017-08-23 04:28:43 +0000

    shot in the dark(literally same bytecode)

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@ import math
 
 class MagicClass:
     """This represents a circle"""
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """Initializes the Magic Class"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
