701652face8f352068fcfa6784f3e716cb4952f8
alexaorrico <alexaorrico@gmail.com>
2017-09-06 00:59:32 +0000

    project 0x0A

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3:
+"""
+
+"""
+class MyList(list):
+    """  """
+    def __init__(self):
+        """  """
+        super().__init__()
+
+    def print_sorted(self):
+        """  """
+        print(sorted(self))
