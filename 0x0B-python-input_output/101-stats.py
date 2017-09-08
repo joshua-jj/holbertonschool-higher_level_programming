1087b779d8b8ae97a21647eb92b0ea52cbf594b5
alexaorrico <alexaorrico@gmail.com>
2017-09-08 04:16:23 +0000

    advanced task 1

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+reads stdin line by line and computes metrics
+"""
+import sys
+
+file_size = 0
+status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+try:
+    for i, line in enumerate(sys.stdin, 1):
+        tokens = line.split()
+        status_tally[tokens[-2]] += 1
+        file_size += int(tokens[-1])
+        if i % 10 == 0:
+            print("File size: {:d}".format(file_size))
+            for key in status_codes:
+                if status_tally[key]:
+                    print("{:s}: {:d}".format(key, status_tally[key]))
+except KeyboardInterrupt:
+    print("File size: {:d}".format(file_size))
+    for key in status_codes:
+        if status_tally[key]:
+            print("{:s}: {:d}".format(key, status_tally[key]))
+    raise
