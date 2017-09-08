22500659048b39c66a34cdaf564317e7d7182cc3
alexaorrico <alexaorrico@gmail.com>
2017-09-08 16:38:50 +0000

    print stats at the end of the file

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -18,6 +18,11 @@ try:
             for key in status_codes:
                 if status_tally[key]:
                     print("{:s}: {:d}".format(key, status_tally[key]))
+    print("File size: {:d}".format(file_size))
+            for key in status_codes:
+                if status_tally[key]:
+                    print("{:s}: {:d}".format(key, status_tally[key]))
+
 except KeyboardInterrupt:
     print("File size: {:d}".format(file_size))
     for key in status_codes:
