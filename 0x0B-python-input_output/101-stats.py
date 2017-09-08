b1eaf51c18455e6b6c5aa5753fcbe9a6be49fe7a
alexaorrico <alexaorrico@gmail.com>
2017-09-08 04:27:22 +0000

    changed enumerate statement

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -9,11 +9,11 @@ status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
                 "403": 0, "404": 0, "405": 0, "500": 0}
 status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
 try:
-    for i, line in enumerate(sys.stdin, 1):
+    for i, line in enumerate(sys.stdin):
         tokens = line.split()
         status_tally[tokens[-2]] += 1
         file_size += int(tokens[-1])
-        if i % 10 == 0:
+        if (i + 1) % 10 == 0:
             print("File size: {:d}".format(file_size))
             for key in status_codes:
                 if status_tally[key]:
