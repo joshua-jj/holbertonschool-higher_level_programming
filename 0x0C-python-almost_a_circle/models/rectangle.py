f5adf5eef39782ea620735ff7aa22d4727f82cb7
alexaorrico <alexaorrico@gmail.com>
2017-09-09 00:47:58 +0000

    first commit

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+"""
+Contains the "Rectangle" class
+"""
+
+from base import Base
+
+
+class Rectangle(Base):
+    """A representation of a rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes the rectangle"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """getter od width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """getter of height"""
+        return self.__height
+
+    @property
+    def x(self):
+        """getter of x"""
+        return self.__x
+
+    @property
+    def y(self):
+        """getter of y"""
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """setter of width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """setter of height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """setter of x"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """setter of y"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """calculates the area of the rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """print a display of the rectangle"""
+        print(("\n" * self.__y) +
+              "\n".join(((" " * self.__x) + ("#" * self.__width))
+                        for i in range(self.__height)))
+
+    def __str__(self):
+        """informal string representation of the rectangle"""
+        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(self.id,
+                                                                 self.__x,
+                                                                 self.__y,
+                                                                 self.__width,
+                                                                 self.__height)
+
+    def update(self, *args, **kwargs):
+        """updates multiple attributes"""
+        for i, a in enumerate(args):
+            if i == 0:
+                self.id = a
+            elif i == 1:
+                self.width = a
+            elif i == 2:
+                self.height = a
+            elif i == 3:
+                self.x = a
+            elif i == 4:
+                self.y = a
+        if "id" in kwargs:
+            self.id = kwargs["id"]
+        if "width" in kwargs:
+            self.width = kwargs["width"]
+        if "height" in kwargs:
+            self.height = kwargs["height"]
+        if "x" in kwargs:
+            self.x = kwargs["x"]
+        if "y" in kwargs:
+            self.y = kwargs["y"]
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains the "Square" class
+"""
+
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A representation of a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializes the square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """informal string representation of the square"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
+                                                         self.y, self.width)
