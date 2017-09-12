914f8cb8ff48386496e4e82fee3259bb8dcf3cf7
alexaorrico <alexaorrico@gmail.com>
2017-09-12 23:36:26 +0000

    testing values before super

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,11 +10,11 @@ class Rectangle(Base):
     """A representation of a rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes the rectangle"""
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
