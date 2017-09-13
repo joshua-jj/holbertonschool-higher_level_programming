ac2b183b2172327ba581fc79ed125ebe466ca05e
alexaorrico <alexaorrico@gmail.com>
2017-09-13 01:36:11 +0000

    bug fix in save_to_file for NoneType arg

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,8 +37,9 @@ class Base:
         """writes the JSON string representation of list_objs to a file"""
         filename = cls.__name__ + ".json"
         lo = []
-        for i in list_objs:
-            lo.append(cls.to_dictionary(i))
+        if list_objs is not None:
+            for i in list_objs:
+                lo.append(cls.to_dictionary(i))
         with open(filename, 'w') as f:
             f.write(cls.to_json_string(lo))
 
