17550a076146ff8020fec9daa020a835656b559b
alexaorrico <alexaorrico@gmail.com>
2017-10-15 19:01:26 +0000

    fix logic

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -2,4 +2,4 @@
 -- If the database hbtn_0c_0 doesn't exist, your script should not fail
 -- You are not allowed to use the SELECT or SHOW statements
 
-DROP DATABASE IF NOT EXISTS hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
