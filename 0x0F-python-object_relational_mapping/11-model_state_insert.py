5a0e822c341333e149698bdd7169e4c7b5f06c6e
alexaorrico <alexaorrico@gmail.com>
2017-10-28 15:53:43 +0000

    remove Base.metadata.create_all(eng)

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -7,14 +7,13 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
+from model_state import Base, State
 
 
 if __name__ == "__main__":
-    from model_state import Base, State
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                                     argv[2],
                                                                     argv[3]))
-    Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     session = Session()
     new_state = State(name='Louisiana')
