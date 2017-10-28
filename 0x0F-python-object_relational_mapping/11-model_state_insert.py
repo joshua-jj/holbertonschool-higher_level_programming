5241bb551700fb8faf603e27884366a0566f577c
alexaorrico <alexaorrico@gmail.com>
2017-10-28 15:55:47 +0000

    revert last commit and remove quotes from docstring

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-adds the State object “Louisiana” to a database
+adds the State object Louisiana to a database
 """
 
 import sqlalchemy
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                                     argv[2],
                                                                     argv[3]))
+    Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     session = Session()
     new_state = State(name='Louisiana')
