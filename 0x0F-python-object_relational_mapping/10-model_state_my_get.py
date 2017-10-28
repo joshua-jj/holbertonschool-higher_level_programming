a334f33243bda66b13a65fc50134e21d26f70d08
alexaorrico <alexaorrico@gmail.com>
2017-10-28 15:15:09 +0000

    pep8 fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,13 +10,13 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                       argv[2],
-                                                                       argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-    state = session.query(State).filter(State.name.like(argv[4])).first()
+    state = session.query(State).filter_by(name=argv[4]).first()
     if state is not None:
         print(str(state.id))
     else:
