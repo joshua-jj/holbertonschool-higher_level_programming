070532d5746e3e9f54f3760abb3b5ebbc711cab5
alexaorrico <alexaorrico@gmail.com>
2017-11-27 11:03:05 +0000

    fix logic error

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,9 +13,11 @@ if __name__ == '__main__':
     r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
         r_dict = r.json()
+        id = r_dict.get('id')
+        name = r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
     except:
         print("Not a valid JSON")
-    if len(r_dict) == 0:
-        print("No result")
-    else:
-        print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
