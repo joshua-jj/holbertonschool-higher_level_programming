7cc281110da5d5382f952e3847cfa01786e7a9d6
alexaorrico <alexaorrico@gmail.com>
2017-12-21 23:39:04 +0000

    semistandard fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 let nextMax = 0;
 let args = process.argv.slice(2);
 if (args.length > 1) {
-  args.sort()
-  nextMax = args[args.length - 2]
+  args.sort();
+  nextMax = args[args.length - 2];
 }
 console.log(nextMax);
