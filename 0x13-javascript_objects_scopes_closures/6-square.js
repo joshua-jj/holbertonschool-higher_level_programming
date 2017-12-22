3d139dde47ef10b6f6b8cbba6acdd37f9d213999
alexaorrico <alexaorrico@gmail.com>
2017-12-22 15:17:05 +0000

    make square inherit from previous square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle.js');
+const Square1 = require('./5-square.js');
 
-module.exports = class Square extends Rectangle {
+module.exports = class Square extends Square1 {
   constructor (size) {
-    super(size, size);
+    super(size);
   }
   charPrint (c) {
     if (c === undefined) {
