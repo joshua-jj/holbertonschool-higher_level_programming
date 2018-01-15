3e1e090f501a03b13a691a3e1c08e5057bc109db
alexaorrico <alexaorrico@gmail.com>
2018-01-15 03:33:19 +0000

    add iife

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,3 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('header').style.color = '#FF0000';
-}, false);
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('header').style.color = '#FF0000';
+  }, false);
+})();
